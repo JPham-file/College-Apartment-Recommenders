25686e9875dfd057b97e490a1af17b98e9f52cfd
JP Pham <63124165+JPham-file@users.noreply.github.com>
2024-02-27 17:29:46 -0600

    added font and changed log in screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,23 +1,48 @@
 import { SafeAreaView, Text, Button, StyleSheet } from 'react-native';
 import { useRouter } from 'expo-router';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    'SpaceGrotesk-Bold': require('../../assets/fonts/SpaceGrotesk-Bold.ttf'),
+  });
 
     const navigation = useRouter();
 
     return (
-        <SafeAreaView style={styles.container}>
-            <Text>Welcome To Off Campus!</Text>
-            <Button title="Sign In" onPress={() => navigation.push("/auth")} />
-        </SafeAreaView>
+      <SafeAreaView style={styles.container}>
+          <Text style={styles.text}>Welcome</Text>
+          <Text style={styles.text}>To</Text>
+          <Text style={styles.text2}>Off Campus!</Text>
+          <Button
+            title="Sign In"
+            onPress={() => navigation.push("/auth")}
+            color="#1E90FF"
+          />
+      </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
+        backgroundColor: "#000000",
+        // alignItems: "center",
         justifyContent: "center",
-    }
+    },
+    text: {
+        color: "#c9c9c9",
+        marginBottom: 20,
+        fontSize: 55,
+        fontWeight: 'bold'
+    },
+    text2: {
+        color: "#FFFFFF",
+        marginBottom: 20,
+        fontFamily: 'SpaceGrotesk-Bold',
+        fontSize: 65,
+        fontWeight: 'bold'
+    },
 });
