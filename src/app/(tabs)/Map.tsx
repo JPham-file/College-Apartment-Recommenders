76c2d544bf99ee63f57066a2747e10a2b9f40618
JP Pham <JPham97@tamu.edu>
2024-04-04 16:00:37 -0500

    added maps to app

--- src/app/(tabs)/Map.tsx
+++ src/app/(tabs)/Map.tsx
@@ -1,13 +1,65 @@
-import { Text, View } from 'react-native';
+import { Text, StyleSheet, View, TouchableOpacity, Dimensions, Image } from 'react-native';
+import { useNavigation } from "@react-navigation/native";
+import * as Location from "expo-location";
+import React, {useEffect, useState} from 'react'
+import MapView, {Marker} from 'react-native-maps';
 
 interface MapPageProps {}
 
 const MapPage = ({}: MapPageProps) => {
+  const [userLocation, setUserLocation] = useState<any>(null)
+  const [initialRegion, setinitialRegion] = useState<any>();
+
+  useEffect(() => {
+    const getLocation = async () => {
+      let {status} = await Location.requestForegroundPermissionsAsync();
+      if ( status !== 'granted') {
+        console.log("permission denied")
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setUserLocation(location.coords);
+      setinitialRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.05,
+        longitudeDelta: 0.05,
+      });
+    };
+
+    getLocation();
+
+  }, []);
+
+
   return (
     <View className="flex flex-row justify-center items-center h-full">
-      <Text className="text-white text-2xl">Coming Soon</Text>
+      {initialRegion && (
+        <MapView style={styles.map} initialRegion={initialRegion}>
+          {userLocation && (
+            <Marker
+              coordinate={{
+                latitude: userLocation.latitude,
+                longitude: userLocation.longitude,
+              }}
+              title="Your Location"
+            />
+          )}
+        </MapView>
+      )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default MapPage;
