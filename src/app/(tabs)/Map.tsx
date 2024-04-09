87c5856235d3af6b1878a644d15a8fc6b4af975e
JP Pham <JPham97@tamu.edu>
2024-04-09 15:57:50 -0500

    added ability to use Google Maps

--- src/app/(tabs)/Map.tsx
+++ src/app/(tabs)/Map.tsx
@@ -2,15 +2,48 @@ import { Text, StyleSheet, View, TouchableOpacity, Dimensions, Image } from 'rea
 import { useNavigation } from "@react-navigation/native";
 import * as Location from "expo-location";
 import React, {useEffect, useState} from 'react'
-import MapView, {Marker} from 'react-native-maps';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import {ApartmentUnitRecommendation} from "@/src/types";
+import {useAuth} from "@clerk/clerk-expo";
 
 interface MapPageProps {}
 
 const MapPage = ({}: MapPageProps) => {
   const [userLocation, setUserLocation] = useState<any>(null)
   const [initialRegion, setinitialRegion] = useState<any>();
+  const [token, setToken] = useState<string | null>(null);
+  const [apartmentData, setApartmentData] = useState<ApartmentUnitRecommendation[]>([]);
+
+  const { getToken } = useAuth();
+
+  const fetchUserPreferences = async () => {
+    const newToken = await getToken();
+    setToken(newToken);
+    try {
+      const apiURL = `${process.env.EXPO_PUBLIC_RECOMMENDATION_API_URL}/get_recommendations`;
+      const response = await fetch(apiURL, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${newToken}`,
+        },
+      });
+      if (!response.ok) {
+        console.error(response)
+        throw new Error('Network response failure: make sure to change IP to your machine IP');
+      }
+      const data = await response.json();
+      // console.log(data)
+      setApartmentData(data)
+    } catch (error) {
+      console.error('There was an error fetching the user preferences:', error);
+    }
+  };
+
+  console.log(apartmentData)
 
   useEffect(() => {
+    fetchUserPreferences();
     const getLocation = async () => {
       let {status} = await Location.requestForegroundPermissionsAsync();
       if ( status !== 'granted') {
@@ -27,7 +60,6 @@ const MapPage = ({}: MapPageProps) => {
         longitudeDelta: 0.05,
       });
     };
-
     getLocation();
 
   }, []);
@@ -36,7 +68,11 @@ const MapPage = ({}: MapPageProps) => {
   return (
     <View className="flex flex-row justify-center items-center h-full">
       {initialRegion && (
-        <MapView style={styles.map} initialRegion={initialRegion}>
+        <MapView
+          style={styles.map}
+          initialRegion={initialRegion}
+          provider={PROVIDER_GOOGLE}
+        >
           {userLocation && (
             <Marker
               coordinate={{
@@ -46,6 +82,13 @@ const MapPage = ({}: MapPageProps) => {
               title="Your Location"
             />
           )}
+          {apartmentData.map((location) => (
+            <Marker
+              key={location.propertyId}
+              coordinate={{ latitude: location.apt_latitude, longitude: location.apt_longitude }}
+              title={location.name}
+            />
+          ))}
         </MapView>
       )}
     </View>
