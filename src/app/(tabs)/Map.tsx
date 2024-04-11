a2fe0d938ccadaa4d3152f27e1fb029e3bb7b9f5
JP Pham <JPham97@tamu.edu>
2024-04-11 16:29:37 -0500

    displays much more information on clickable markers

--- src/app/(tabs)/Map.tsx
+++ src/app/(tabs)/Map.tsx
@@ -1,10 +1,12 @@
-import { Text, StyleSheet, View, TouchableOpacity, Dimensions, Image } from 'react-native';
+import { FlatList, Modal, Text, StyleSheet, View, TouchableOpacity, Dimensions, Image } from 'react-native';
 import { useNavigation } from "@react-navigation/native";
 import * as Location from "expo-location";
 import React, {useEffect, useState} from 'react'
-import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, {Callout, Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import {ApartmentUnitRecommendation} from "@/src/types";
 import {useAuth} from "@clerk/clerk-expo";
+import ApartmentItem from '@/src/components/ApartmentItem';
+
 
 interface MapPageProps {}
 
@@ -33,14 +35,26 @@ const MapPage = ({}: MapPageProps) => {
         throw new Error('Network response failure: make sure to change IP to your machine IP');
       }
       const data = await response.json();
-      // console.log(data)
-      setApartmentData(data)
+      const maxScore = data[0].score;
+      let transformedApartments = data.map((apartment: ApartmentUnitRecommendation) => ({
+        ...apartment,
+        match: Number((apartment.score / maxScore) * 100).toFixed(0).toString(),
+      }));
+      setApartmentData(transformedApartments)
+
+      if (Array.isArray(transformedApartments)) { // Check if data is an array
+        transformedApartments.forEach((item, index) => {
+          // console.log(`Item ${index}:`, item['name'], item['propertyId']);
+        });
+      } else {
+        console.log(transformedApartments); // If not an array, log the whole response
+      }
+
+
     } catch (error) {
       console.error('There was an error fetching the user preferences:', error);
     }
   };
-
-  console.log(apartmentData)
 
   useEffect(() => {
     fetchUserPreferences();
@@ -64,6 +78,31 @@ const MapPage = ({}: MapPageProps) => {
 
   }, []);
 
+  const [coordinates] = useState([
+    {
+      latitude: 48.8587741,
+      longitude: 2.2069771,
+    },
+    {
+      latitude: 48.8323785,
+      longitude: 2.3361663,
+    },
+  ]);
+
+  const [showModal, setShowModal] = useState(false);
+  const [selectedApartmentData, setSelectedApartmentData] = useState<ApartmentUnitRecommendation[]>([]);
+  const handleMarkerPress = (aptPropertyID : any) => {
+    console.log(aptPropertyID)
+    // setSelectedApartmentData(apartmentData);
+    const aptUnit : any = [];
+    apartmentData.forEach((item, index) => {
+      if (item.propertyId === aptPropertyID) {
+        aptUnit.push(item)
+      }
+    });
+    setSelectedApartmentData(aptUnit);
+    setShowModal(true);
+  }
 
   return (
     <View className="flex flex-row justify-center items-center h-full">
@@ -73,6 +112,16 @@ const MapPage = ({}: MapPageProps) => {
           initialRegion={initialRegion}
           provider={PROVIDER_GOOGLE}
         >
+
+          {/*<MapViewDirections*/}
+          {/*  origin={coordinates[0]}*/}
+          {/*  destination={coordinates[1]}*/}
+          {/*  apikey={PROVIDER_GOOGLE} // insert your API Key here*/}
+          {/*  strokeWidth={4}*/}
+          {/*  strokeColor="#111111"*/}
+          {/*/>*/}
+
+
           {userLocation && (
             <Marker
               coordinate={{
@@ -82,15 +131,57 @@ const MapPage = ({}: MapPageProps) => {
               title="Your Location"
             />
           )}
-          {apartmentData.map((location) => (
+
+          <Marker
+            coordinate={{
+              latitude:30.6187,
+              longitude:-96.3365,
+            }}
+            title="Texas A&M University"
+          />
+
+          {apartmentData.map((apartmentUnit, index) => (
             <Marker
-              key={location.propertyId}
-              coordinate={{ latitude: location.apt_latitude, longitude: location.apt_longitude }}
-              title={location.name}
-            />
+              key={apartmentUnit.propertyId + index}
+              coordinate={{ latitude: apartmentUnit.apt_latitude, longitude: apartmentUnit.apt_longitude }}
+              title={apartmentUnit.name}
+            >
+              <Callout tooltip style={styles.callout} onPress={ () => handleMarkerPress(apartmentUnit.propertyId) }>
+                <View style={styles.calloutView}>
+                  <Text>{apartmentUnit.name}</Text>
+                  <Text>{apartmentUnit.address}</Text>
+                  <Text>{`Matched: ${apartmentUnit.match}`}</Text>
+                  {/*<Text>{`Rent: $${apartmentUnit.rent}.00`}</Text>*/}
+                  <Text>{`User Rating: ${apartmentUnit.rating}/5`}</Text>
+                  <Text>Tap for more details</Text>
+                </View>
+              </Callout>
+            </Marker>
           ))}
+
+
         </MapView>
       )}
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={showModal}
+        onRequestClose={() => setShowModal(!showModal)}
+      >
+        <View style={styles.modalView}>
+          <FlatList
+            data={selectedApartmentData}
+            keyExtractor={(item) => item.propertyId.toString()}
+            renderItem={({ item }) => (
+              <ApartmentItem apartment={item} token={token} isSkeletonLoading={false} showScore={true} />
+            )}
+          />
+          <TouchableOpacity style={styles.closeButton} onPress={() => setShowModal(false)}>
+            <Text style={styles.closeButtonText}>Close</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -103,6 +194,59 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
+  modalView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+    backgroundColor: 'white', // Set a background color for better visibility
+    borderRadius: 20,
+    padding: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  closeButton: {
+    backgroundColor: '#2196F3',
+    padding: 10,
+    borderRadius: 10,
+    marginTop: 20,
+  },
+  closeButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  callout: {
+    position: 'absolute',
+    minWidth: 250,
+    zIndex: 5,
+  },
+  calloutView: {
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 6,
+    backgroundColor: 'white',
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+  calloutTitle: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  calloutDescription: {
+    fontSize: 14,
+  },
+  calloutPrice: {
+    fontSize: 14,
+    color: 'red',
+  },
 });
-
 export default MapPage;
