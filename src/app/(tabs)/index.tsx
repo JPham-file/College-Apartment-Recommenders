e831e9c70602be8004f3ca6ef6d7fd26315d3325
haydenholt <haydenholt60485@gmail.com>
2024-04-23 15:44:31 -0500

    refresh fix

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -118,7 +118,7 @@ export default function TabOneScreen() {
         renderItem={renderApartmentItem}
         onEndReached={loadMore}
         onEndReachedThreshold={0.5}
-        refreshControl={
+                refreshControl={
           <RefreshControl
             refreshing={refreshing}
             onRefresh={onRefresh}
